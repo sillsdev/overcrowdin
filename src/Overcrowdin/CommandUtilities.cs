90e69375923fed8680004afbcc6a2c01d8b6ac4f
Hasso <hasso_pape@sil.org>
2019-12-04 14:25:58 -0600

    partial: use globs to find files
    - find files in recursive subdirectories
    - helpful error for unsupported globs
    - partial: use relative paths for files' keys
    - clarify some existing unit tests

--- src/Overcrowdin/CommandUtilities.cs
+++ src/Overcrowdin/CommandUtilities.cs
@@ -1,16 +1,40 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Abstractions;
+using System.Linq;
 using Microsoft.Extensions.Configuration;
 
 namespace Overcrowdin
 {
 	public static class CommandUtilities
 	{
+		private const string UnsupportedSyntaxX =
+			"The specified source syntax is not supported. Please submit a pull request to help us support ";
+
+		public static Dictionary<string, FileInfo> GetFileList(IConfiguration config, IFileOptions opts, IFileSystem fs)
+		{
+			var files = new Dictionary<string, FileInfo>();
+			// handle files specified on the command line
+			if (opts.Files != null && opts.Files.Any())
+			{
+				foreach (var file in opts.Files)
+				{
+					files[file] = new FileInfo(file); // TODO (Hasso) 2019.12: use the full relative path here and elsewhere. Centralize.
+				}
+			}
+			else
+			{
+				GetFilesFromConfiguration(config, fs, files);
+			}
+
+			return files;
+		}
+
 		/// <summary>Read from configuration files section that resembles:
 		/// files : [
 		///  {
-		///    "source" : "resources/en/*.json",
+		///    "source" : "resources/en/**/*.json",
 		///    "translation" : "resources/%two_letters_code%/%original_file_name"
 		///  }
 		/// ]
@@ -21,11 +45,31 @@ namespace Overcrowdin
 			var filesSection = config.GetSection("files");
 			foreach (IConfigurationSection section in filesSection.GetChildren())
 			{
-				var filePattern = section.GetValue<string>("source");
-				var matchedFiles = fs.Directory.GetFiles(fs.Directory.GetCurrentDirectory(), filePattern);
+				var source = section.GetValue<string>("source");
+				var basePath = fs.Directory.GetCurrentDirectory();
+				var basePathLength = basePath.Length;
+				var directory = Path.Combine(basePath, Path.GetDirectoryName(source));
+				var searchOption = SearchOption.TopDirectoryOnly;
+				if (directory.Contains("***"))
+				{
+					throw new NotImplementedException(UnsupportedSyntaxX + source);
+				}
+				if (directory.EndsWith("**"))
+				{
+					// For now, we support recursion only at the end of the directory path. Other options are not worth our effort at this time.
+					directory = Path.GetDirectoryName(directory);
+					searchOption = SearchOption.AllDirectories;
+				}
+				if (directory.Contains('*'))
+				{
+					throw new NotImplementedException(UnsupportedSyntaxX + source);
+				}
+				var filePattern = Path.GetFileName(source);
+				var matchedFiles = fs.Directory.GetFiles(directory, filePattern, searchOption);
 				foreach (var sourceFile in matchedFiles)
 				{
-					files[Path.GetFileName(sourceFile)] = new FileInfo(sourceFile);
+					// two off-by-one errors cancel each other out to cleanly trim the path separator from the key
+					files[sourceFile.Substring(basePathLength)] = new FileInfo(sourceFile);
 				}
 			}
 		}
