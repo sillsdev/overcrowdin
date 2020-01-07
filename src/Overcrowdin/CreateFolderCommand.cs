a979bb53869e00e1db80d45adcafcb9a3b08e742
Hasso <hasso_pape@sil.org>
2020-01-07 14:48:29 -0600

    Add Create Folder command
    - (not accessible from the command line; to be called by Add and Update Files)
    - implements https://github.com/sillsdev/overcrowdin/issues/14
    - required by https://jira.sil.org/browse/LT-20026

---
+++ src/Overcrowdin/CreateFolderCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO.Abstractions;
+using System.Threading;
+using System.Threading.Tasks;
+using Crowdin.Api;
+using Crowdin.Api.Typed;
+using Microsoft.Extensions.Configuration;
+
+namespace Overcrowdin
+{
+	public sealed class CreateFolderCommand
+	{
+		public static async Task<int> CreateFolderInCrowdin(IConfiguration config, GlobalOptions opts, string folder, AutoResetEvent gate, IFileSystem fs)
+		{
+			var crowdin = CrowdinCommand.GetClient();
+			var projectId = config["project_identifier"];
+			var projectKey = Environment.GetEnvironmentVariable(config["api_key_env"]);
+			var projectCredentials = new ProjectCredentials { ProjectKey = projectKey };
+			var createFolderParams = new CreateFolderParameters {Name = folder};
+			if (opts.Verbose)
+			{
+				Console.WriteLine("Creating folder {0}...", folder);
+			}
+			var result = await crowdin.CreateFolder(projectId, projectCredentials, createFolderParams);
+			if (result.IsSuccessStatusCode)
+			{
+				if (opts.Verbose)
+				{
+					Console.WriteLine("Finished creating folder.");
+					var info = await result.Content.ReadAsStringAsync();
+					Console.WriteLine(info);
+				}
+			}
+			else
+			{
+				Console.WriteLine("Failure creating folder.");
+				if (opts.Verbose)
+				{
+					var error = await result.Content.ReadAsStringAsync();
+					Console.WriteLine(error);
+				}
+			}
+			gate.Set();
+			return result.IsSuccessStatusCode ? 0 : 1;
+		}
+	}
+}
