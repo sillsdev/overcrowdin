50106646a884dd6649d111a8c07bedd23f87c52e
Hasso <hasso_pape@sil.org>
2020-01-07 14:48:29 -0600

    Add Create Folder command
    - (not accessible from the command line; to be called by Add and Update Files)
    - implements https://github.com/sillsdev/overcrowdin/issues/14
    - required by https://jira.sil.org/browse/LT-20026

---
+++ OvercrowdinTests/CreateFolderCommandTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO.Abstractions.TestingHelpers;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Crowdin.Api;
+using Crowdin.Api.Typed;
+using Moq;
+using Overcrowdin;
+using Xunit;
+
+namespace OvercrowdinTests
+{
+	public class CreateFolderCommandTests : CrowdinApiTestBase
+	{
+		[Fact]
+		public async void CreateFolder()
+		{
+			var mockFileSystem = new MockFileSystem();
+			const string newFolderName = "testDir";
+			const string apiKeyEnvVar = "KEYEXISTS";
+			const string projectId = "testcrowdinproject";
+			Environment.SetEnvironmentVariable(apiKeyEnvVar, "fakecrowdinapikey");
+			_mockConfig.Setup(config => config["api_key_env"]).Returns(apiKeyEnvVar);
+			_mockConfig.Setup(config => config["project_identifier"]).Returns(projectId);
+			// Set up only the expected call to CreateFolder (any calls without the expected folder params will return null)
+			_mockClient.Setup(x => x.CreateFolder(It.IsAny<string>(), It.IsAny<ProjectCredentials>(), It.Is<CreateFolderParameters>(fp => newFolderName.Equals(fp.Name))))
+				.Returns(Task.FromResult(new HttpResponseMessage(HttpStatusCode.Accepted)));
+			var gate = new AutoResetEvent(false);
+			var opts = new GlobalOptions();
+			var result = await CreateFolderCommand.CreateFolderInCrowdin(_mockConfig.Object, opts, newFolderName, gate, mockFileSystem);
+			gate.WaitOne();
+			_mockClient.Verify();
+			Assert.Equal(0, result);
+		}
+	}
+}
