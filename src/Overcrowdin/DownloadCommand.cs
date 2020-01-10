e46fec2cc6c19fd98e52de104e2fc2bb0758233c
papeh <hasso_pape@sil.org>
2020-01-10 16:06:41 -0600

    Report any errors downloading translations (#21)

--- src/Overcrowdin/DownloadCommand.cs
+++ src/Overcrowdin/DownloadCommand.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.IO.Abstractions;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using CommandLine;
@@ -18,7 +19,8 @@ namespace Overcrowdin
 			[Option('l', Required = false, Default ="all", HelpText = "The language to download the translations for or 'all' to download for every language.")]
 			public string Language { get; set; }
 
-			[Option('e', Required = false, Default = false, HelpText = "Export all translations before downloading. This generates the .zip files on the Crowdin server.")]
+			[Option('e', Required = false, Default = false, HelpText = "Export all translations before downloading. This generates the .zip files" +
+				"\non the Crowdin server. If false, the most-recently-exported translations will be downloaded.")]
 			public bool ExportFirst { get; set; }
 
 			[Option('f', Required = true, HelpText = "Path and filename relative to the configured basepath for the zip file.")]
@@ -30,7 +32,7 @@ namespace Overcrowdin
 		{
 			var crowdin = CrowdinCommand.GetClient();
 
-			var success = 1;
+			var status = 1;
 			var projectId = config["project_identifier"];
 			var projectKey = Environment.GetEnvironmentVariable(config["api_key_env"]);
 			var projectCredentials = new ProjectCredentials { ProjectKey = projectKey };
@@ -40,13 +42,12 @@ namespace Overcrowdin
 			{
 				try
 				{
-					var exportFailed = false;
+					dynamic exportResponse = new {IsSuccessStatusCode = true};
 					if (opts.ExportFirst)
 					{
-						var exportResponse = await crowdin.ExportTranslation(projectId, projectCredentials, new ExportTranslationParameters());
-						exportFailed = !exportResponse.IsSuccessStatusCode;
+						exportResponse = await crowdin.ExportTranslation(projectId, projectCredentials, new ExportTranslationParameters());
 					}
-					if (!exportFailed)
+					if (exportResponse.IsSuccessStatusCode)
 					{
 						var downloadResponse = await crowdin.DownloadTranslation(projectId,
 							projectCredentials, new DownloadTranslationParameters { Package = opts.Language });
@@ -56,8 +57,31 @@ namespace Overcrowdin
 							{
 								downloadedFile.Write(await downloadResponse.Content.ReadAsByteArrayAsync());
 							}
+							Console.WriteLine("Translations file downloaded to {0}", outputFile);
+							status = 0;
 						}
-						success = 0;
+						else
+						{
+							Console.WriteLine("Failed to download translations.");
+							if (downloadResponse.StatusCode == HttpStatusCode.NotFound && !opts.ExportFirst)
+							{
+								Console.WriteLine("Did you export your translations?");
+							}
+							if (opts.Verbose)
+							{
+								var error = await downloadResponse.Content.ReadAsStringAsync();
+								Console.WriteLine(error);
+							}
+						}
+					}
+					else
+					{
+						Console.WriteLine("Failed to export translations.");
+						if (opts.Verbose)
+						{
+							var error = await exportResponse.Content.ReadAsStringAsync();
+							Console.WriteLine(error);
+						}
 					}
 				}
 				catch (CrowdinException e)
@@ -65,18 +89,17 @@ namespace Overcrowdin
 					Console.WriteLine("Failed to download translations. Check your project id and project key.");
 					if (opts.Verbose)
 					{
-						Console.WriteLine(e.ErrorCode);
+						Console.WriteLine($"{e.ErrorCode}: {e.Message}");
 						Console.WriteLine(e.StackTrace);
 					}
 				}
-				Console.WriteLine("Translations file downloaded to {0}", outputFile);
 			}
 			else
 			{
 				Console.WriteLine("{0} did not contain the API Key for your Crowdin project.", config["api_key_env"]);
 			}
 			gate.Set();
-			return success;
+			return status;
 		}
 	}
 }
