ab6af45ab2294c0153f1d52156b9e3f5725c6134
Hasso <hasso_pape@sil.org>
2020-01-10 11:23:37 -0600

    Report any errors downloading translations

--- OvercrowdinTests/DownloadCommandTests.cs
+++ OvercrowdinTests/DownloadCommandTests.cs
@@ -43,19 +43,20 @@ namespace OvercrowdinTests
 			_mockConfig.Setup(config => config["api_key_env"]).Returns(apiKeyEnvVar);
 			_mockConfig.Setup(config => config["project_identifier"]).Returns(projectId);
 			_mockConfig.Setup(config => config["base_path"]).Returns(baseDir);
-			// Setup the calls to Export and Download
-			_mockClient.Setup(client => client.ExportTranslation(projectId,It.IsAny<ProjectCredentials>(),
-				It.IsAny<ExportTranslationParameters>())).Returns(Task.FromResult(new HttpResponseMessage(HttpStatusCode.Accepted)));
+			// Set up the calls to Export and Download
+			_mockClient.Setup(client => client.ExportTranslation(projectId, It.IsAny<ProjectCredentials>(),
+					It.IsAny<ExportTranslationParameters>()))
+				.Returns(Task.FromResult(new HttpResponseMessage(HttpStatusCode.Accepted)));
 			_mockClient.Setup(client => client.DownloadTranslation(projectId, It.IsAny<ProjectCredentials>(),
-				It.IsAny<DownloadTranslationParameters>()))
-				.Returns(Task.FromResult(new HttpResponseMessage(HttpStatusCode.Accepted) { Content = new ByteArrayContent(new byte [] { 0x50, 0x4b, 0x03, 0x04 })}));
+					It.IsAny<DownloadTranslationParameters>()))
+				.Returns(Task.FromResult(new HttpResponseMessage(HttpStatusCode.Accepted) {Content = new ByteArrayContent(new byte[] {0x50, 0x4b, 0x03, 0x04})}));
 			mockFileSystem.Directory.CreateDirectory(baseDir);
 			var gate = new AutoResetEvent(false);
 			var result = await DownloadCommand.DownloadFromCrowdin(_mockConfig.Object, new DownloadCommand.Options { ExportFirst = true, Filename = outputFileName },
 				gate, mockFileSystem.FileSystem);
 			gate.WaitOne();
-			_mockClient.Verify(x => x.ExportTranslation(projectId, It.IsAny<ProjectCredentials>(), It.IsAny<ExportTranslationParameters>()));
-			_mockClient.Verify(x => x.DownloadTranslation(projectId, It.IsAny<ProjectCredentials>(), It.IsAny<DownloadTranslationParameters>()));
+			_mockClient.Verify(x => x.ExportTranslation(projectId, It.IsAny<ProjectCredentials>(), It.IsAny<ExportTranslationParameters>()), Times.Exactly(1));
+			_mockClient.Verify(x => x.DownloadTranslation(projectId, It.IsAny<ProjectCredentials>(), It.IsAny<DownloadTranslationParameters>()), Times.Exactly(1));
 			Assert.Equal(0, result);
 		}
 
@@ -71,7 +72,7 @@ namespace OvercrowdinTests
 			_mockConfig.Setup(config => config["api_key_env"]).Returns(apiKeyEnvVar);
 			_mockConfig.Setup(config => config["project_identifier"]).Returns(projectId);
 			_mockConfig.Setup(config => config["base_path"]).Returns(baseDir);
-			// Setup the call to Download
+			// Set up the call to Download
 			_mockClient.Setup(client => client.DownloadTranslation(projectId, It.IsAny<ProjectCredentials>(),
 					It.IsAny<DownloadTranslationParameters>()))
 				.Returns(Task.FromResult(new HttpResponseMessage(HttpStatusCode.Accepted) { Content = new ByteArrayContent(new byte[] { 0x50, 0x4b, 0x03, 0x04 }) }));
@@ -83,6 +84,30 @@ namespace OvercrowdinTests
 			_mockClient.Verify(x => x.ExportTranslation(projectId, It.IsAny<ProjectCredentials>(), It.IsAny<ExportTranslationParameters>()), Times.Never);
 			_mockClient.Verify(x => x.DownloadTranslation(projectId, It.IsAny<ProjectCredentials>(), It.IsAny<DownloadTranslationParameters>()), Times.Exactly(1));
 			Assert.Equal(0, result);
+		}
+
+		[Fact]
+		public async void ErrorsAreReported()
+		{
+			var mockFileSystem = new MockFileSystem();
+			const string outputFileName = "test.zip";
+			const string apiKeyEnvVar = "EXPORTKEYFORTEST";
+			const string projectId = "testcrowdinproject";
+			const string baseDir = "test";
+			Environment.SetEnvironmentVariable(apiKeyEnvVar, "fakeapikey");
+			_mockConfig.Setup(config => config["api_key_env"]).Returns(apiKeyEnvVar);
+			_mockConfig.Setup(config => config["project_identifier"]).Returns(projectId);
+			_mockConfig.Setup(config => config["base_path"]).Returns(baseDir);
+			var options = new DownloadCommand.Options {ExportFirst = false, Filename = outputFileName};
+			// Set up the call to Download
+			_mockClient.Setup(client => client.DownloadTranslation(projectId, It.IsAny<ProjectCredentials>(), It.IsAny<DownloadTranslationParameters>()))
+				.Returns(Task.FromResult(new HttpResponseMessage(HttpStatusCode.NotFound)));
+			mockFileSystem.Directory.CreateDirectory(baseDir);
+			var gate = new AutoResetEvent(false);
+			var result = await DownloadCommand.DownloadFromCrowdin(_mockConfig.Object, options, gate, mockFileSystem.FileSystem);
+			gate.WaitOne();
+			_mockClient.Verify(x => x.DownloadTranslation(projectId, It.IsAny<ProjectCredentials>(), It.IsAny<DownloadTranslationParameters>()), Times.Exactly(1));
+			Assert.Equal(1, result);
 		}
 	}
 }
