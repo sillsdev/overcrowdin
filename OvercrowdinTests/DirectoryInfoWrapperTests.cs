51af3e6e3ded5622b5907a2fe2344554ac44aee9
Hasso <hasso_pape@sil.org>
2020-01-28 11:36:49 -0600

    Implement Globbing DirectoryInfoWrapper
    The two provided implementations of DirectoryInfoBase are inadequate.
    DirectoryInfoWrapper doesn't accept IDirectoryInfo, which we use for tests.
    InMemoryDirectoryInfo doesn't work for the base directory edge case (C:\) and could be inefficient for us.

---
+++ OvercrowdinTests/DirectoryInfoWrapperTests.cs
@@ -0,0 +1,109 @@
+using System.IO;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using Xunit;
+using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
+using IFileSystem = System.IO.Abstractions.IFileSystem;
+using DirectoryInfoWrapper = Overcrowdin.DirectoryInfoWrapper;
+
+namespace OvercrowdinTests
+{
+	public class DirectoryInfoWrapperTests
+	{
+		private static readonly IFileSystem FileSystem = new MockFileSystem();
+
+		[Theory]
+		[InlineData(@"C:", @"C:\")]
+		[InlineData(@"C:\testDir\", @"C:\testDir")]
+		public static void FullName(string input, string normalized)
+		{
+			var sut = new DirectoryInfoWrapper(FileSystem.DirectoryInfo.FromDirectoryName(input));
+			Assert.Equal(normalized, sut.FullName);
+		}
+
+		[Theory]
+		[InlineData(@"C:\", "")]
+		[InlineData(@"C:\superDir\subDir", "subDir")]
+		public static void Name(string fullName, string name)
+		{
+			var sut = new DirectoryInfoWrapper(FileSystem.DirectoryInfo.FromDirectoryName(fullName));
+			Assert.Equal(name, sut.Name);
+		}
+
+		[Theory]
+		[InlineData(@"C:\", null)]
+		[InlineData(@"C:\superDir\subDir", @"C:\superDir")]
+		public static void ParentDirectory(string path, string parent)
+		{
+			var sut = new DirectoryInfoWrapper(FileSystem.DirectoryInfo.FromDirectoryName(path));
+			Assert.Equal(parent, sut.ParentDirectory?.FullName);
+		}
+
+		[Fact]
+		public static void GetDirectory()
+		{
+			const string superDir = @"C:\superDir";
+			const string subDir = "subDir";
+			var sut = new DirectoryInfoWrapper(FileSystem.DirectoryInfo.FromDirectoryName(superDir));
+			var result = sut.GetDirectory(subDir);
+			Assert.Equal(Path.Combine(superDir, subDir), result.FullName);
+			// The subdirectory is not required to exist. It does not in this test, and it should not be created.
+			Assert.False(FileSystem.Directory.Exists(result.FullName));
+		}
+
+		[Fact]
+		public static void GetFile()
+		{
+			const string directory = @"C:\dir";
+			const string file = "file.txt";
+			var sut = new DirectoryInfoWrapper(FileSystem.DirectoryInfo.FromDirectoryName(directory));
+			var result = sut.GetFile(file);
+			Assert.Equal(Path.Combine(directory, file), result.FullName);
+		}
+
+		[Fact]
+		public static void EnumerateFileSystemInfos()
+		{
+			var fileSys = new MockFileSystem();
+			fileSys.Directory.CreateDirectory("jane/doe");
+			fileSys.Directory.CreateDirectory("john/doe");
+			fileSys.Directory.CreateDirectory("john/quincy/adams");
+			fileSys.Directory.CreateDirectory("john/quincy/doe");
+			fileSys.File.WriteAllText("test.txt", "contents");
+			fileSys.File.WriteAllText("jane/test.txt", "contents");
+			fileSys.File.WriteAllText("jane/doe/test.txt", "contents");
+			fileSys.File.WriteAllText("john/test.txt", "contents");
+			fileSys.File.WriteAllText("john/doe/test.txt", "contents");
+			fileSys.File.WriteAllText("john/quincy/test.txt", "contents");
+			fileSys.File.WriteAllText("john/quincy/adams/test.txt", "contents");
+			fileSys.File.WriteAllText("john/quincy/adams/allonym.txt", "contents");
+			fileSys.File.WriteAllText("john/quincy/doe/test.txt", "contents");
+
+			var sut = new DirectoryInfoWrapper(fileSys.DirectoryInfo.FromDirectoryName("/"));
+			var results = sut.EnumerateFileSystemInfos().ToArray();
+
+			Assert.Equal(3, results.Length);
+			Assert.Contains(results, info => IsFileInfoMatching(info, @"C:\test.txt"));
+			Assert.Contains(results, info => IsDirInfoMatching(info, @"C:\jane"));
+
+			var john = (DirectoryInfoBase)results.First(info => IsDirInfoMatching(info, @"C:\john"));
+			Assert.Equal(3, john.EnumerateFileSystemInfos().Count());
+
+			var adams = john.GetDirectory("quincy").GetDirectory("adams");
+			var adamses = adams.EnumerateFileSystemInfos().ToArray();
+			Assert.Equal(2, adamses.Length);
+			Assert.Contains(adamses, info => IsFileInfoMatching(info, @"C:\john\quincy\adams\test.txt"));
+			Assert.Contains(adamses, info => IsFileInfoMatching(info, @"C:\john\quincy\adams\allonym.txt"));
+		}
+
+		private static bool IsFileInfoMatching(FileSystemInfoBase info, string path)
+		{
+			return info is FileInfoBase && info.FullName.Equals(path);
+		}
+
+		private static bool IsDirInfoMatching(FileSystemInfoBase info, string path)
+		{
+			return info is DirectoryInfoBase && info.FullName.Equals(path);
+		}
+	}
+}
---
+++ src/Overcrowdin/DirectoryInfoWrapper.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
+using IDirectoryInfo = System.IO.Abstractions.IDirectoryInfo;
+using IFileInfo = System.IO.Abstractions.IFileInfo;
+
+namespace Overcrowdin
+{
+	public class DirectoryInfoWrapper : DirectoryInfoBase
+	{
+		private readonly IDirectoryInfo _directoryInfo;
+
+		public override string Name => _directoryInfo.Name;
+		public override string FullName => _directoryInfo.FullName;
+
+		public DirectoryInfoWrapper(IDirectoryInfo directoryInfo)
+		{
+			_directoryInfo = directoryInfo;
+		}
+
+		public override DirectoryInfoBase ParentDirectory
+		{
+			get
+			{
+				var parent = _directoryInfo.Parent;
+				return parent == null ? null : new DirectoryInfoWrapper(parent);
+			}
+		}
+
+		public override IEnumerable<FileSystemInfoBase> EnumerateFileSystemInfos()
+		{
+			foreach (var info in _directoryInfo.EnumerateFileSystemInfos())
+			{
+				switch (info)
+				{
+					case IFileInfo fileInfo:
+						yield return new FileInfoWrapper(new FileInfo(fileInfo.FullName));
+						break;
+					case IDirectoryInfo dirInfo:
+						yield return new DirectoryInfoWrapper(dirInfo);
+						break;
+				}
+			}
+		}
+
+		public override DirectoryInfoBase GetDirectory(string path)
+		{
+			var info = _directoryInfo.FileSystem.DirectoryInfo.FromDirectoryName(Path.Combine(FullName, path));
+			return new DirectoryInfoWrapper(info);
+		}
+
+		public override FileInfoBase GetFile(string path)
+		{
+			return new FileInfoWrapper(new FileInfo(Path.Combine(FullName, path)));
+		}
+	}
+}
