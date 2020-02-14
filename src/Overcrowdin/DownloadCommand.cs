7bf36f4c88a17734ccfb18c080264d57c863cc9a
Hasso <hasso_pape@sil.org>
2020-02-14 16:33:32 -0600

    Retry failed downloads
    * Retry failed downloads the specified number of times (option -r) at 4-minute intervals
    * Failed exports, or attempting to download translations that do not seem to have been exported, still results in immediate failure.

--- src/Overcrowdin/DownloadCommand.cs
+++ src/Overcrowdin/DownloadCommand.cs
@@ -25,81 +25,118 @@ namespace Overcrowdin
 
 			[Option('f', Required = true, HelpText = "Path and filename relative to the configured basepath for the zip file.")]
 			public string Filename { get; set; }
+
+			[Option('r', Required = false, Default = 0, HelpText = "Number of times to retry failed downloads (in four-minute intervals)")]
+			public int Retries { get; set; }
 		}
 
+		/// <summary>time to wait before retrying failed downloads (four minutes = 240,000ms)</summary>
+		private const int RetryWaitTime = 240000;
+
+		/// <summary>Number of milliseconds in a minute (60,000ms)</summary>
+		private const int MillisPerMinute = 60000;
 
 		public static async Task<int> DownloadFromCrowdin(IConfiguration config, Options opts, AutoResetEvent gate, IFileSystem fs)
 		{
-			var crowdin = CrowdinCommand.GetClient();
-
-			var status = 1;
-			var projectId = config["project_identifier"];
-			var projectKey = Environment.GetEnvironmentVariable(config["api_key_env"]);
-			var projectCredentials = new ProjectCredentials { ProjectKey = projectKey };
-
-			var outputFile = Path.Combine(config["base_path"], opts.Filename);
-			if (!string.IsNullOrEmpty(projectKey))
+			try
 			{
+				var crowdin = CrowdinCommand.GetClient();
+
+				var projectId = config["project_identifier"];
+				var projectKey = Environment.GetEnvironmentVariable(config["api_key_env"]);
+				var projectCredentials = new ProjectCredentials {ProjectKey = projectKey};
+
+				var outputFile = Path.Combine(config["base_path"], opts.Filename);
+				if (string.IsNullOrEmpty(projectKey))
+				{
+					Console.WriteLine("{0} did not contain the API Key for your Crowdin project.", config["api_key_env"]);
+					return 1;
+				}
 				try
 				{
-					dynamic exportResponse = new {IsSuccessStatusCode = true};
 					if (opts.ExportFirst)
 					{
-						exportResponse = await crowdin.ExportTranslation(projectId, projectCredentials, new ExportTranslationParameters());
-					}
-					if (exportResponse.IsSuccessStatusCode)
-					{
-						var downloadResponse = await crowdin.DownloadTranslation(projectId,
-							projectCredentials, new DownloadTranslationParameters { Package = opts.Language });
-						if (downloadResponse.IsSuccessStatusCode)
+						var exportResponse = await crowdin.ExportTranslation(projectId, projectCredentials,
+							new ExportTranslationParameters());
+						if (!exportResponse.IsSuccessStatusCode)
 						{
-							using (var downloadedFile = fs.FileStream.Create(outputFile, FileMode.Create))
-							{
-								downloadedFile.Write(await downloadResponse.Content.ReadAsByteArrayAsync());
-							}
-							Console.WriteLine("Translations file downloaded to {0}", outputFile);
-							status = 0;
-						}
-						else
-						{
-							Console.WriteLine("Failed to download translations.");
-							if (downloadResponse.StatusCode == HttpStatusCode.NotFound && !opts.ExportFirst)
-							{
-								Console.WriteLine("Did you export your translations?");
-							}
+							Console.WriteLine("Failed to export translations.");
 							if (opts.Verbose)
 							{
-								var error = await downloadResponse.Content.ReadAsStringAsync();
+								var error = await exportResponse.Content.ReadAsStringAsync();
 								Console.WriteLine(error);
 							}
-						}
-					}
-					else
-					{
-						Console.WriteLine("Failed to export translations.");
-						if (opts.Verbose)
-						{
-							var error = await exportResponse.Content.ReadAsStringAsync();
-							Console.WriteLine(error);
+							return 1;
 						}
 					}
 				}
 				catch (CrowdinException e)
 				{
-					Console.WriteLine("Failed to download translations. Check your project id and project key.");
+					Console.WriteLine("Failed to export translations. Check your project id and project key.");
 					if (opts.Verbose)
 					{
 						Console.WriteLine($"{e.ErrorCode}: {e.Message}");
 						Console.WriteLine(e.StackTrace);
 					}
 				}
+
+				for (var retries = opts.Retries; retries >= 0; retries --)
+				{
+					try
+					{
+						var downloadResponse = await crowdin.DownloadTranslation(projectId,
+							projectCredentials, new DownloadTranslationParameters {Package = opts.Language});
+						if (downloadResponse.IsSuccessStatusCode)
+						{
+							using (var downloadedFile = fs.FileStream.Create(outputFile, FileMode.Create))
+							{
+								downloadedFile.Write(await downloadResponse.Content.ReadAsByteArrayAsync());
+							}
+
+							Console.WriteLine("Translations file downloaded to {0}", outputFile);
+							return 0;
+						}
+						Console.WriteLine("Failed to download translations.");
+						if (downloadResponse.StatusCode == HttpStatusCode.NotFound && !opts.ExportFirst)
+						{
+							Console.WriteLine("Did you export your translations?");
+							if (opts.Verbose)
+							{
+								var error = await downloadResponse.Content.ReadAsStringAsync();
+								Console.WriteLine(error);
+							}
+							return 1;
+						}
+
+						if (opts.Verbose)
+						{
+							var error = await downloadResponse.Content.ReadAsStringAsync();
+							Console.WriteLine(error);
+						}
+					}
+					catch (CrowdinException e)
+					{
+						Console.WriteLine("Failed to download translations. Check your project id and project key.");
+						if (opts.Verbose)
+						{
+							Console.WriteLine($"{e.ErrorCode}: {e.Message}");
+							Console.WriteLine(e.StackTrace);
+						}
+					}
+
+					if (retries > 0)
+					{
+						Console.WriteLine($"Trying {retries} more times in {RetryWaitTime/MillisPerMinute}-minute intervals.");
+						Thread.Sleep(RetryWaitTime);
+					}
+				}
+
+				return 1;
 			}
-			else
+			finally
 			{
-				Console.WriteLine("{0} did not contain the API Key for your Crowdin project.", config["api_key_env"]);
+				gate.Set();
 			}
-			gate.Set();
-			return status;
 		}
 	}
 }
