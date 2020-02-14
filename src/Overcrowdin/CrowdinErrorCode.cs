d652442eb4b92eb8f610b538e37ce31ebdc65495
Hasso <hasso_pape@sil.org>
2020-02-14 16:33:32 -0600

    Retry failed downloads
    * Retry failed downloads the specified number of times (option -r) at 4-minute intervals
    * Failed exports, attempting to download translations that do not seem to have been exported, and other seemingly permanent failures, still result in immediate failure.

--- src/Overcrowdin/CrowdinErrorCode.cs
+++ src/Overcrowdin/CrowdinErrorCode.cs
@@ -6,7 +6,28 @@ namespace Overcrowdin
 	public enum CrowdinErrorCode
 	{
 		// ENHANCE (Hasso) 2020.01: add a method to extract these from an HttpResponseMessage (see CreateFolderCommand for sample code)
+		/// <summary>"success" XML returned from Crowdin</summary>
+		Success = -2, // chosen to avoid a collision with "unknown internal error"
+		/// <summary>Unable to parse XML, or the Root element was neither "success" nor "error"</summary>
+		NoError = -1, // chosen to avoid a collision with "unknown internal error"
+		UnknownInternalError = 0,
+		ProjectDoesntExistOrInvalidKey = 1,
+		UnknownApiAction = 2,
+		InvalidKey = 3,
+		AccountDoesntExist = 11,
+		InvalidAccountKey = 12,
+		NoUserIdSpecified = 18,
+		UserNatFound = 20,
+		InvalidParameters = 27,
+		NoUsernameSpecified = 37,
+		InvalidParameterValues = 38,
+		PermissionDenied = 41,
+		ProjectSuspended = 43,
+		NoUserIdOrUsername = 44,
 		DirectoryAlreadyExists = 50,
-		DirectoryNameHasInvalidCharacters = 51
+		DirectoryNameHasInvalidCharacters = 51,
+		ProjectWasNotBuilt = 54, // the build was canceled or interrupted
+		NoStringsFound = 56,
+		NoTranslationsFound = 57
 	}
 }
